7b146d6d9d46dbfe23ee7679d50caff811751770
Vamsi Krishna Kodimela <55780006+vamsi-krishna-kodimela@users.noreply.github.com>
2024-06-06 18:05:12 +0000

    chore: Update main layout component to add overflow hidden and padding
    feat: Update player screen component to include player component
    
    chore: Add styles for player component
    
    feat: Update player component to include video player and controls

--- decentralized-streaming-app/src/app/components/player/player.component.ts
+++ decentralized-streaming-app/src/app/components/player/player.component.ts
@@ -1,12 +1,84 @@
-import { Component } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Component, InputSignal, ViewChild, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, DecimalPipe],
   templateUrl: './player.component.html',
-  styleUrl: './player.component.less'
+  styleUrl: './player.component.less',
 })
 export class PlayerComponent {
+  videoUrl: InputSignal<string> = input.required<string>();
+  isMuted: boolean = false;
+  volume: number = 1;
+  progress: number = 0;
+  isPlaying: boolean = false;
+  duration: number = 0;
+  currentTime: number = 0;
 
+  @ViewChild('videoPlayer') videoPlayer: any;
+
+  ngAfterViewInit() {
+    this.videoPlayer.nativeElement.addEventListener(
+      'timeupdate',
+      this.updateProgress.bind(this)
+    );
+    this.videoPlayer.nativeElement.addEventListener(
+      'ended',
+      this.handleVideoEnd.bind(this)
+    );
+  }
+
+  setVolume() {
+    this.videoPlayer.nativeElement.volume = this.volume;
+  }
+
+  setProgress() {
+    this.duration = this.videoPlayer.nativeElement.duration;
+    this.videoPlayer.nativeElement.currentTime =
+      (this.progress / 100) * this.duration;
+    this.currentTime = this.videoPlayer.nativeElement.currentTime;
+  }
+
+  muteUnmute() {
+    this.isMuted = !this.isMuted;
+    this.videoPlayer.nativeElement.muted = this.isMuted;
+  }
+
+  playPause() {
+    this.isPlaying = !this.isPlaying;
+    if (this.isPlaying) {
+      this.videoPlayer.nativeElement.play();
+    } else {
+      this.videoPlayer.nativeElement.pause();
+    }
+  }
+
+  updateProgress() {
+    this.currentTime = this.videoPlayer.nativeElement.currentTime;
+    this.duration = this.videoPlayer.nativeElement.duration;
+    this.progress = (this.currentTime / this.duration) * 100;
+
+    console.log(
+      `Current Time: ${this.currentTime} Duration: ${this.duration} Progress: ${this.progress}`
+    );
+  }
+
+  handleVideoEnd() {
+    this.isPlaying = false;
+    this.progress = 0;
+  }
+  openFullScreen() {
+    if (this.videoPlayer.nativeElement.requestFullscreen) {
+      this.videoPlayer.nativeElement.requestFullscreen();
+    } else if (this.videoPlayer.nativeElement.mozRequestFullScreen) {
+      this.videoPlayer.nativeElement.mozRequestFullScreen();
+    } else if (this.videoPlayer.nativeElement.webkitRequestFullscreen) {
+      this.videoPlayer.nativeElement.webkitRequestFullscreen();
+    } else if (this.videoPlayer.nativeElement.msRequestFullscreen) {
+      this.videoPlayer.nativeElement.msRequestFullscreen();
+    }
+  }
 }
--- decentralized-streaming-app/src/app/screens/player-screen/player-screen.component.ts
+++ decentralized-streaming-app/src/app/screens/player-screen/player-screen.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { PlayerComponent } from "../../components/player/player.component";
 
 @Component({
-  selector: 'app-player-screen',
-  standalone: true,
-  imports: [],
-  templateUrl: './player-screen.component.html',
-  styleUrl: './player-screen.component.less'
+    selector: 'app-player-screen',
+    standalone: true,
+    templateUrl: './player-screen.component.html',
+    styleUrl: './player-screen.component.less',
+    imports: [PlayerComponent]
 })
 export class PlayerScreenComponent {
 
