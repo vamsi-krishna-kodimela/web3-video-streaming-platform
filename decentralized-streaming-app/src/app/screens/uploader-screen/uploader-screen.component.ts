f78ef80c668cabd298de0ee9008783a774d2dad7
Vamsi Krishna Kodimela <55780006+vamsi-krishna-kodimela@users.noreply.github.com>
2024-06-08 05:08:20 +0000

    Uploader screen

--- decentralized-streaming-app/src/app/screens/uploader-screen/uploader-screen.component.ts
+++ decentralized-streaming-app/src/app/screens/uploader-screen/uploader-screen.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-uploader-screen',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './uploader-screen.component.html',
-  styleUrl: './uploader-screen.component.less'
+  styleUrl: './uploader-screen.component.less',
 })
 export class UploaderScreenComponent {
-
+  title: string = '';
+  description: string = '';
+  onSubmit($event: SubmitEvent) {
+    throw new Error('Method not implemented.');
+  }
+  onFileChange($event: Event) {
+    throw new Error('Method not implemented.');
+  }
 }
